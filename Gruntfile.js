727f0378f15b4d0f3d52d8ff8add8197426d70f8
Noah Sloan <nsloan@spredfast.com>
2014-10-08 12:31:03 -0500

    browserify the tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,13 +40,42 @@ module.exports = function(grunt) {
         src: "src/mu.js",
         dest: "dist/mu.prod.js"
       },
-    }
+      tests: {
+        options: {
+          bundleOptions: {
+            debug: true,
+          }
+        },
+        src: "test/suite.js",
+        dest: "dist/browserified-tests.js"
+      },      
+    },
+    watch: {
+      tests: {          
+        files: ["test/**/*.js", "src/**/*.js"],
+        tasks: ["browserify:tests"],
+        options: {
+          livereload: true,
+        },
+      },
+    },
+    connect: {
+      tests: {
+        options: {
+          port: 2345,
+          livereload: true,
+        }
+      }
+    },    
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask('tests', ["browserify:tests", 'connect:tests', 'watch:tests']);  
 
   // Default task.
   grunt.registerTask('default', ['simplemocha:all', "browserify:mu", "browserify:muProd"]);
